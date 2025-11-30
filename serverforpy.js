d658bf2cb9ec7ecaaa7fc7dc4c2b46c9bd8b60bc
Md. Humaian Kabir <humaian2@gmail.com>
2025-11-30 11:48:04 +0600

    Its an nodeJs server that calls fastapi server

---
+++ serverforpy.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+const axios = require('axios')
+
+app.get('/user', (req, res) => {
+    const url = "http://localhost:8000/items/34"
+
+    axios.get(url)
+        .then(response => {
+            console.log('response data:', response.data)
+        })
+
+        .catch(error => {
+            console.error('Error fetching data:', error.message)
+        })
+
+        res.send("ok")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000...")
+})
