24dcf960a07f73a58eb2dba3d9e99d7bf9b1e23e
Md. Humaian Kabir <humaian2@gmail.com>
2025-11-30 19:44:12 +0600

    For my practice

--- server3.js
+++ server3.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
 
-app.get('/fetch-fastapi', async (req, res) => {
-    try {
-        const response = await axios.get('http://localhost:8000/');
-        res.json(response.data); // Send fastapi response to the client
-    }
+const axios = require('axios')
 
-    catch (error) {
-        // console.error('Error calling FastAPI:', error.message);
-        res.status(500).json({error: error.message});
-    }
-});
-// start the server
-app.listen(port, () => {
-    console.log('Node.js server running on http://localhost:3000');
-});
+app.get('/user', (req, res) => {
+
+    const url = "http://localhost:8000/items/34/Kabir"
+    axios.get(url)
+        .then(response => {
+            console.log("response data:", response.data)
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error.message)
+        })
+
+    res.send("ok")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000...")
+})
